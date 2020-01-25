c32ce464eb470c6bd121056f7b9c49674ee3eac0
satriai0805 <satriai418@gmail.com>
2020-01-25 18:28:59 +0800

    Laravel 6 API Fix
    Tugas Web Lanjut

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,17 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::apiResource('artikel','ArtikelAPIController');
+Route::apiResource('kategoriArtikel','KategoriArtikelAPIController');
+
+Route::apiResource('berita','BeritaAPIController');
+Route::apiResource('kategoriBerita','KategoriBeritaAPIController');
+
+Route::apiResource('galeri','GaleriAPIController');
+Route::apiResource('kategoriGaleri','KategoriGaleriAPIController');
+
+Route::apiResource('pengumuman','PengumumanAPIController');
+Route::apiResource('kategoriPengumuman','KategoriPengumumanAPIController');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
